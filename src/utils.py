0bf9b15e3e48d4b97b76d40bd9770aad22b3c99c
klinga <klingaroo@gmail.com>
2021-05-21 10:49:17 -0400

    repo structure

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+import csv
+
+
+def save2csv(dst_fh, row):
+    """
+    Appends a list with data to a dst_fh csv
+    args:
+        dst_fh: str, output file
+        row: list, list of values to write in a row
+    """
+
+    with open(dst_fh, "a", encoding="utf-8") as csvfile:
+        out = csv.writer(
+            csvfile,
+            delimiter=",",
+            lineterminator="\n",
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL,
+        )
+        try:
+            out.writerow(row)
+        except UnicodeEncodeError:
+            pass
+
+
+def save2marc(dst_fh, record):
+    with open(dst_fh, "ab") as marcfile:
+        marcfile.write(record.as_marc())
