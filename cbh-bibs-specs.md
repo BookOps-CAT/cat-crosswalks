0de3a52f407d770e4c26a111d489bffe63d3e6b5
Tomek <klingaroo@gmail.com>
2022-03-28 14:44:06 -0400

    init create bibs

---
+++ cbh-bibs-specs.md
@@ -0,0 +1,12 @@
+## Specification for CBH records exported from ArchivesSpace to Worldcat
+
++ tag 040 $aNyBlHS$beng$cBKL
++ retain multiple 300 tags
++ construct 264$c based on 245$f
++ remove double line breaks in subfields 351, 500 and 544.
++ remove existing 506 tag
++ add 506 1 $a Collection is open to the public, may only be used in the library and is not available through interlibrary loan. Library policy on photocopying will apply. Advance notice may be required.  
+
+
+### Notes
++ unable to use encoding level "M" (409 HTTP error: Not authorized to use M in Encoding Level (Leader/17).)
--- src/as2worldcat.py
+++ src/as2worldcat.py
@@ -30,22 +30,13 @@ Example of the validation error response from Metadata service:
   <id>http://worldcat.org/oclc/</id>
 </entry>
 """
+import os
+import json
 
-from pymarc import MARCReader
-from pymarc.marcxml import record_to_xml
+from pymarc import Field, MARCReader, Record
+from pymarc.marcxml import record_to_xml, parse_xml_to_array
 
-
-def fix_and_save_to_marc(xml_file: str):
-    reader = parse_xml_to_array(xml_file)
-    marcfile = f"{xml_file[:-4]}-PRC.mrc"
-    for record in reader:
-        # manipulate leader of each record
-        record.leader = f"{record.leader[:17]}ii{record.leader[19:]}"
-        record["035"].indicators = [" ", " "]
-        record["040"].subfields = ["a", "BKL", "b", "eng", "e", "rda", "c", "BKL"]
-
-        with open(marcfile, "ab") as out:
-            out.write(record.as_marc())
+from bookops_worldcat import WorldcatAccessToken, MetadataSession
 
 
 def marc2xml(marcfile: str):
@@ -56,6 +47,138 @@ def marc2xml(marcfile: str):
             yield xmlstr
 
 
+def xml2marc(xmlfile: str) -> Record:
+    """
+    Serializes MARC XML to pymarc `Record` object
+    for manipulation
+    """
+    with open(xmlfile, "r") as f:
+        reader = parse_xml_to_array(f)
+        for record in reader:
+            yield record
+
+
+def has_invalid_subfields(subfields):
+    invalid = False
+    for s in subfields:
+        if "|" in s:
+            invalid = True
+            break
+    return invalid
+
+
+def manipulate_as_record(record: Record) -> None:
+    """
+    Manipulations are done inplace
+    """
+    record.leader = f"{record.leader[:17]}Ki{record.leader[19:]}"
+
+    # fix indicators in 035
+    record["035"].indicators = [" ", " "]
+
+    record["040"].subfields = ["a", "NyBlHS", "b", "eng", "e", "dacs", "c", "BKL"]
+
+    dates = record["245"]["f"].strip()
+    record.add_ordered_field(
+        Field(tag="264", indicators=[" ", "0"], subfields=["c", dates])
+    )
+
+    for field in record.get_fields("351"):
+        new_subfields = ["a"]
+        value = field["b"]
+        value = value.replace("\n\n", " ")
+        new_subfields.append(value)
+
+        field.subfields = new_subfields
+
+    for field in record.get_fields("500"):
+        new_subfields = [s.replace("\n\n", " ") for s in field.subfields]
+        field.subfields = new_subfields
+
+    record.remove_fields("506")
+    record.add_ordered_field(
+        Field(
+            tag="506",
+            indicators=["1", " "],
+            subfields=[
+                "a",
+                "Collection is open to the public, may only be used in the library and is not available through interlibrary loan. Library policy on photocopying will apply. Advance notice may be required.",
+            ],
+        )
+    )
+
+    for field in record.get_fields("544"):
+        new_subfields = []
+        value = field["n"]
+        value = value.split("\n\n")
+        n = len(value)
+        i = 0
+        for v in value:
+            i += 1
+            new_subfields.append("d")
+            if i == n:
+                new_subfields.append(v)
+            else:
+                new_subfields.append(f"{v};")
+
+        # remove last semicolon
+        field.subfields = new_subfields
+
+    # fix LCSH subject subfield coding
+    subjects = record.subjects()
+    for term in subjects:
+        if term.indicator2 == "0":
+            if has_invalid_subfields(term.subfields):
+                print(term.subfields)
+                new_subfields = ["a"]
+                malformed = term.subfields[1]
+                sub_list = malformed.split("|")
+                main = sub_list[0].strip()
+                new_subfields.append(main)
+                for s in sub_list[1:]:
+                    sub = s[0]
+                    new_sub = s[1:].strip()
+                    new_subfields.append(sub)
+                    new_subfields.append(new_sub)
+                term.subfields = new_subfields
+
+    return record
+
+
+def save2marc(record, marcfile):
+    with open(marcfile, "ab") as out:
+        out.write(record.as_marc())
+
+
+def get_token():
+    creds_fh = os.path.join(os.environ["USERPROFILE"], ".oclc/bpl_overload.json")
+    with open(creds_fh, "r") as f:
+        cred = json.load(f)
+        token = WorldcatAccessToken(
+            key=cred["key"],
+            secret=cred["secret"],
+            scopes="WorldCatMetadataAPI",
+            principal_id=cred["principal_id"],
+            principal_idns=cred["principal_idns"],
+            agent="tomaszkalata@bookops.org",
+        )
+    return token
+
+
 if __name__ == "__main__":
-    fh = "./files/BCMS.0082_20210930_203437_UTC__marc21.xml"
-    fix_and_save_to_marc(fh)
+
+    marcxml = "./files/CBH/2019_004-marc.xml"
+    marcfile = f"{marcxml[:-4]}-PRC.mrc"
+    reader = xml2marc(marcxml)
+    for record in reader:
+        bib = manipulate_as_record(record)
+        print(bib)
+        save2marc(bib, marcfile)
+
+    token = get_token()
+    with MetadataSession(authorization=token) as session:
+        reader = marc2xml(marcfile)
+        for record in reader:
+            result = session.create_bib(inst="13437", instSymbol="BKL", xmldata=record)
+            print(result.status_code)
+            print(result.content)
